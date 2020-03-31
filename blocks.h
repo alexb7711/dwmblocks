30b76226c29a926c9d1b4034c8a21161fee8c8e5
Alex Brown <alex.brown7711@gmail.com>
2020-03-31 07:55:15 -0600

    Statusbar for dwm

--- blocks.h
+++ blocks.h
@@ -1,19 +1,24 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
+	// {"P:", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+        // {"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+        {" CPU:", "~/Code/dwmblocks/scripts/cpu_usage",	30,		0},
+        {"Memory:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "~/bin/statusbar/volume",						0,		10},
+	// {"V:", "~/bin/statusbar/volume",						0,		10},
 
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
+	// {"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
 	
-	{"", "~/bin/statusbar/battery",						5,		0},
 
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+	// {"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+	// {"T:", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+        {"", "~/Code/dwmblocks/scripts/bandwidth",				5,		4},
+        {"", "~/Code/dwmblocks/scripts/volume",				5,		4},
 
-	{"", "~/bin/statusbar/clock",						5,		0},
+        {"Battery:", "~/Code/dwmblocks/scripts/battery | tr \'\n\' \' \' ",						5,		0},
+        {"", "date '+%D|%I:%m %p '",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
