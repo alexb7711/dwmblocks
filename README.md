2b949fd34e5a121a0cf188decf0cdd3140288324
Alex Brown <alex.brown7711@gmail.com>
2020-04-03 08:24:08 -0600

    bandwidth scripts has some bash-isms, cleaned up header file, updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# dwmblocks
-Modular status bar for dwm written in c.
-# modifying blocks
-The statusbar is made from text output from commandline programs.
-Blocks are added and removed by editing the blocks.h header file.
+# Introduction
+This is my build of dwmblocks. It is a tool to modify the statis bar for dwm. It has modules for:
+1. CPU usage
+2. Memory Usage
+3. Internet bandwidth in and out
+4. Volume level (Still a work in progress)
+5. Battery
+6. Date
+7. Time
+
+# What is dwmblocks?
+As described by torrinfail:
+
+> Modular status bar for dwm written in c.
+> # modifying blocks
+> The statusbar is made from text output from commandline programs.
+> Blocks are added and removed by editing the blocks.h header file.
+
+# Dependencies
+All you need is to have dwm installed. 
+
+# Running dwmblocks
+In your `~/.xinitrc` file put: 
+
+``` bash
+dwmblocks 2>&1 >/dev/null &
+```
--- blocks.h
+++ blocks.h
@@ -1,24 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	// {"P:", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-        // {"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-        {" CPU:", "~/Code/dwmblocks/scripts/cpu_usage",	30,		0},
-        {"Memory:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	// {"V:", "~/bin/statusbar/volume",						0,		10},
-
-	// {"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-
-	// {"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-	// {"T:", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-        {"", "~/Code/dwmblocks/scripts/bandwidth",				5,		4},
-        {"", "~/Code/dwmblocks/scripts/volume",				5,		4},
-
-        {"Battery:", "~/Code/dwmblocks/scripts/battery | tr \'\n\' \' \' ",						5,		0},
-        {"", "date '+%D|%I:%m %p '",						5,		0},
+        {" CPU:"    , "~/Code/dwmblocks/scripts/cpu_usage"                      , 5  , 0}  ,
+        {"Memory:"  , "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g" , 30 , 0}  ,
+        {""         , "~/Code/dwmblocks/scripts/bandwidth"                      , 5  , 4}  ,
+        {""         , "~/Code/dwmblocks/scripts/volume"                         , 5  , 10} ,
+        {"Battery:" , "~/Code/dwmblocks/scripts/battery | tr \'\n\' \' \' "     , 5  , 0}  ,
+        {""         , "date '+%D|%I:%M %p '"                                    , 60 , 0}  ,
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
