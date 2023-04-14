b122c5e066d4a2e1f40a87de25240962370060b3
Alexander Brown <alex.brown7711@gmail.com>
2023-04-14 09:14:16 -0600

    Update 4/14/23

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {" :" , "~/Code/Suckless/dwmblocks/scripts/timerunning"                , 60 , 0} ,
-        {"﬙:"  , "~/Code/Suckless/dwmblocks/scripts/cpu_usage"                  , 5  , 0} ,
+        {" :" , "~/code/suckless/dwmblocks/scripts/timerunning"                , 60 , 0} ,
+        {"﬙:"  , "~/code/suckless/dwmblocks/scripts/cpu_usage"                  , 5  , 0} ,
         {":"  , "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g"      , 30 , 0} ,
-        {":"  , "~/Code/Suckless/dwmblocks/scripts/bandwidth"                  , 5  , 4} ,
-        {"墳:" , "~/Code/Suckless/dwmblocks/scripts/volume"                     , 5  , 0} ,
-        {":"  , "~/Code/Suckless/dwmblocks/scripts/battery | tr \'\n\' \' \' " , 5  , 0} ,
-        {":"  , "~/Code/Suckless/dwmblocks/scripts/clock"                      , 5  , 0} ,
+        {":"  , "~/code/suckless/dwmblocks/scripts/bandwidth"                  , 5  , 4} ,
+        {"墳:" , "~/code/suckless/dwmblocks/scripts/volume"                     , 5  , 0} ,
+        {":"  , "~/code/suckless/dwmblocks/scripts/battery | tr \'\n\' \' \' " , 5  , 0} ,
+        {":"  , "~/code/suckless/dwmblocks/scripts/clock"                      , 5  , 0} ,
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = '|';
+static char delim[] = "|";
 static unsigned int delimLen = 5;
