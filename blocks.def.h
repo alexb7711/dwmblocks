5a457fd809a29fe2f8e416c74b6a74c9cf79bd74
Alexander Brown <alex.brown7711@gmail.com>
2023-05-21 07:23:03 -0600

    remove invalid ascii

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {" :" , "~/code/suckless/dwmblocks/scripts/timerunning"                , 60 , 0} ,
-        {"﬙:"  , "~/code/suckless/dwmblocks/scripts/cpu_usage"                  , 5  , 0} ,
-        {":"  , "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g"      , 30 , 0} ,
-        {":"  , "~/code/suckless/dwmblocks/scripts/bandwidth"                  , 5  , 4} ,
-        {"墳:" , "~/code/suckless/dwmblocks/scripts/volume"                     , 5  , 0} ,
-        {":"  , "~/code/suckless/dwmblocks/scripts/battery | tr \'\n\' \' \' " , 5  , 0} ,
-        {":"  , "~/code/suckless/dwmblocks/scripts/clock"                      , 5  , 0} ,
+        {"" , "~/code/suckless/dwmblocks/scripts/timerunning"                , 60 , 0} ,
+        {""  , "~/code/suckless/dwmblocks/scripts/cpu_usage"                  , 5  , 0} ,
+        {""  , "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g"      , 30 , 0} ,
+        {""  , "~/code/suckless/dwmblocks/scripts/bandwidth"                  , 5  , 4} ,
+        {"" , "~/code/suckless/dwmblocks/scripts/volume"                     , 5  , 0} ,
+        /* {":"  , "~/code/suckless/dwmblocks/scripts/battery | tr \'\n\' \' \' " , 5  , 0} , */
+        {""  , "~/code/suckless/dwmblocks/scripts/clock"                      , 5  , 0} ,
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
